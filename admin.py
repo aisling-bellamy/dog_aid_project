df0c49c95fa34eb7b6f208d472e34cfff7471e76
aisling-bellamy <66953727+aisling-bellamy@users.noreply.github.com>
2020-07-28 17:50:17 +0100

    Delete admin.py

--- admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from dog_aid.models import UserProfile, DogOwner, DogProfile
-
-admin.site.register(UserProfile)
-admin.site.register(DogOwner)
-admin.site.register(DogProfile)
