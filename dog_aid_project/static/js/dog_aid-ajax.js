f685b5333a84ec5827eac8176131753e3d47802a
aisling-bellamy <66953727+aisling-bellamy@users.noreply.github.com>
2020-09-24 19:57:30 +0100

    Add files via upload

--- dog_aid_project/static/js/dog_aid-ajax.js
+++ dog_aid_project/static/js/dog_aid-ajax.js
@@ -1,1 +1,15 @@
+$('#suggestion').keyup(function(){
+    var query;
+    query = $(this).val();
+    $.get('/dog_aid/suggest/', {suggestion: query}, function(data){
+        $('#illness_suggest').html(data);
+    });
+});
 
+$('#emergency_suggestion').keyup(function(){
+    var query;
+    query = $(this).val();
+    $.get('/dog_aid/emergency_suggest/', {emergency_suggestion: query}, function(data){
+        $('#emergency_suggest').html(data);
+    });
+});
---
+++ dog_aid_project/static/js/dog_aid-jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("p").hover(function() {
+    $(this).css('color', 'red');
+    },
+    function() {
+    $(this.css('color', 'black');
+    });
+});
